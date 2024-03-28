64f7b8a1c69a9d272a6d2691f796ed09a113bb75
Bilal Ahmed <bilalkietacc@gmail.com>
2024-03-29 00:57:23 +0500

    basic styling

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
+    let { title, desc, imageurl } = this.props;
     return (
-      <div>
-        news item component
+      <div className="container d-flex justify-content-center">
+        <div className="card" style={{ width: "22rem" }}>
+          <img src={imageurl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{desc}</p>
+            <div className="d-flex justify-content-center">
+              <a href="#" className="btn bt-sm btn-primary ">
+                Read more
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
