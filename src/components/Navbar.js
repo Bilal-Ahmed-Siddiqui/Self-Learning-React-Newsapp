663bdbe614ec260301e5511f43a4c264d57c6f07
Bilal Ahmed <bilalkietacc@gmail.com>
2024-04-01 02:37:50 +0500

    categories and countries drop down UI added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,154 @@
 import React, { Component } from "react";
 
 export default class navbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: {
+        general: "general",
+        business: "business",
+        entertainment: "entertainment",
+        health: "health",
+        science: "science",
+        sports: "sports",
+        technology: "technology",
+      },
+      countries: {
+        ae: "United Arab Emirates",
+        ar: "Argentina",
+        at: "Austria",
+        au: "Australia",
+        be: "Belgium",
+        bg: "Bulgaria",
+        br: "Brazil",
+        ca: "Canada",
+        ch: "Switzerland",
+        cn: "China",
+        co: "Colombia",
+        cu: "Cuba",
+        cz: "Czech Republic",
+        de: "Germany",
+        eg: "Egypt",
+        fr: "France",
+        gb: "United Kingdom",
+        gr: "Greece",
+        hk: "Hong Kong",
+        hu: "Hungary",
+        id: "Indonesia",
+        ie: "Ireland",
+        il: "Israel",
+        in: "India",
+        it: "Italy",
+        jp: "Japan",
+        kr: "South Korea",
+        lt: "Lithuania",
+        lv: "Latvia",
+        ma: "Morocco",
+        mx: "Mexico",
+        my: "Malaysia",
+        ng: "Nigeria",
+        nl: "Netherlands",
+        no: "Norway",
+        nz: "New Zealand",
+        ph: "Philippines",
+        pl: "Poland",
+        pt: "Portugal",
+        ro: "Romania",
+        rs: "Serbia",
+        ru: "Russia",
+        sa: "Saudi Arabia",
+        se: "Sweden",
+        sg: "Singapore",
+        si: "Slovenia",
+        sk: "Slovakia",
+        th: "Thailand",
+        tr: "Turkey",
+        tw: "Taiwan",
+        ua: "Ukraine",
+        us: "United States",
+        ve: "Venezuela",
+        za: "South Africa",
+      },
+    };
+  }
+
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg bg-dark navbar-dark ">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              Rabbit News
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    About
-                  </a>
-                </li>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Rabbit News
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+            </ul>
+            <div className="dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Select Category
+              </button>
+              <ul className="dropdown-menu">
+                  {Object.keys(this.state.categories).map((element) => {
+                    return (
+                      <li key={element}>
+                        <a className="dropdown-item" href="#">
+                        {this.state.categories[element]}
+                      </a>
+                      </li>
+                    );
+                  })}
               </ul>
             </div>
+            <div className="dropdown mx-5">
+  <button
+    className="btn btn-secondary dropdown-toggle"
+    type="button"
+    data-bs-toggle="dropdown"
+    aria-expanded="false"
+  >
+    Select Country
+  </button>
+  <ul className="dropdown-menu" style={{ maxHeight: '200px', overflowY: 'auto' }}>
+    {Object.keys(this.state.countries).map((element) => {
+      return (
+        <li key={element}>
+          <a className="dropdown-item" href="#">
+            {this.state.countries[element]}
+          </a>
+        </li>
+      );
+    })}
+  </ul>
+</div>
+
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
